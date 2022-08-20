016620b32ea28df5d8faab171577e01d04d97eca
sebin-pavus <sebinpavus@gmail.com>
2022-08-20 17:02:43 +0530

    Updated repo

---
+++ internal/web/server.go
@@ -0,0 +1,16 @@
+package web
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sebin-pavus/sample/internal/web/handler"
+)
+
+func Init() {
+	handler.Init()
+}
+
+func NewServer(router *gin.Engine) {
+	router.GET("/albums", handler.GetAlbums)
+	router.GET("/albums/:id", handler.GetAlbumByID)
+	router.POST("/albums", handler.PostAlbums)
+}
